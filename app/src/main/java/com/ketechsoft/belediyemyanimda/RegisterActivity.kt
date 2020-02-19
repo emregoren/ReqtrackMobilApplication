9917ae22c7241953af0426b36b0a7dbbcabdc7c4
Emre GOREN <emregrnn58@gmail.com>
2020-02-19 12:20:19 +0300

    Login screen and Regisgter screen added

--- app/src/main/java/com/ketechsoft/belediyemyanimda/RegisterActivity.kt
+++ app/src/main/java/com/ketechsoft/belediyemyanimda/RegisterActivity.kt
@@ -3,11 +3,10 @@ package com.ketechsoft.belediyemyanimda
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_register)
     }
 }
